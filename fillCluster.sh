d68d090d0d08a9a4e45ebec1e311d4c90eaf0015
John Harrigan <jharriga@redhat.com>
2018-05-09 15:12:36 -0400

    Create fillCluster.sh

---
+++ fillCluster.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# FillCLUSTER.sh
+
+myPath="${BASH_SOURCE%/*}"
+if [[ ! -d "$myPath" ]]; then
+    myPath="$PWD" 
+fi
+
+# Variables
+source "$myPath/vars.shinc"
+
+rawUsed=`ceph df | head -n 3 | tail -n 1 | awk '{print $4}'`
+pendingGC=`radosgw-admin gc list --include-all | wc -l`
+echo "Starting statistics:"
+echo "   %RAW USED ${rawUsed} : Pending GCs ${pendingGC}" 
+
+
+# Run the COSbench workload to fill the cluster
+echo "starting the I/O workload to fill the Ceph cluster"
+./Utils/cos.sh "${myPath}/fillWorkload.xml"
+
+echo "$PROGNAME: Done"	
+
+rawUsed=`ceph df | head -n 3 | tail -n 1 | awk '{print $4}'`
+pendingGC=`radosgw-admin gc list --include-all | wc -l`
+echo "Ending statistics:"
+echo "   %RAW USED ${rawUsed} : Pending GCs ${pendingGC}" 
+
+
+# DONE
