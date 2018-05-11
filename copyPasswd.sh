27724b9d85af4376722af14dce9d7cee145cf74e
John Harrigan <jharriga@redhat.com>
2018-05-11 16:11:04 -0400

    Create copyPasswd.sh

---
+++ copyPasswd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# COPYPASSWD.sh
+
+myPath="${BASH_SOURCE%/*}"
+if [[ ! -d "$myPath" ]]; then
+    myPath="$PWD" 
+fi
+
+# Variables
+source "$myPath/vars.shinc"
+
+# Edit the Password into the XML workload files
+echo "inserting password into XML files $FILLxml, $EMPTYxml, $RUNTESTxml"
+key=$(ssh $RGWhostname 'radosgw-admin user info --uid=johndoe | grep secret_key' | tail -1 | awk '{print $2}' | sed 's/"//g')
+sed  -i "s/password=.*;/password=$key;/g" ${FILLxml}
+sed  -i "s/password=.*;/password=$key;/g" ${EMPTYxml}
+sed  -i "s/password=.*;/password=$key;/g" ${RUNTESTxml}
+
+echo "$PROGNAME: Done"	
+
+# DONE
