5a2d2cd2b0506212bc19f56f696fd4cb075235e0
John Harrigan <jharriga@redhat.com>
2018-05-11 15:57:37 -0400

    Update emptyCluster.sh

--- emptyCluster.sh
+++ emptyCluster.sh
@@ -9,24 +9,44 @@ fi
 # Variables
 source "$myPath/vars.shinc"
 
-rawUsed=`ceph df | head -n 3 | tail -n 1 | awk '{print $4}'`
-pendingGC=`radosgw-admin gc list --include-all | wc -l`
-echo "Starting statistics:"
-echo "   %RAW USED ${rawUsed} : Pending GCs ${pendingGC}" 
-date
+# Functions
+source "$myPath/Utils/functions.shinc"
 
+# Create log file - named in vars.shinc
+if [ ! -d $RESULTSDIR ]; then
+  mkdir -p $RESULTSDIR || \
+    error_exit "$LINENO: Unable to create RESULTSDIR."
+fi
+touch $LOGFILE || error_exit "$LINENO: Unable to create LOGFILE."
+updatelog "${PROGNAME} - Created logfile: $LOGFILE" $LOGFILE
+
+# Record STARTING cluster capacity and GC stats
+var1=`echo; ceph df | head -n 5`
+var2=`echo; ceph df | grep rgw.buckets.data`
+updatelog "$var1$var2" $LOGFILE
+# Record GC stats
+var3=`radosgw-admin gc list --include-all | wc -l`
+updatelog "Pending GC's == $var3" $LOGFILE
+#### ALTERNATE STATS METHOD
+#rawUsed=`ceph df | head -n 3 | tail -n 1 | awk '{print $4}'`
+#pendingGC=`radosgw-admin gc list --include-all | wc -l`
+#date
+#echo "Starting statistics:"
+#echo "   %RAW USED ${rawUsed} : Pending GCs ${pendingGC}" 
+
+updatelog "START: cosbench launched" $LOGFILE
 
 # Run the COSbench workload to empty the cluster
 echo "starting the I/O workload to empty the Ceph cluster"
-./Utils/cos.sh "${myPath}/emptyWorkload.xml"
+./Utils/cos.sh ${myPath}/${EMPTYxml} $LOGFILE
 
-echo "$PROGNAME: Done"	
+# Record ENDING cluster capacity and GC stats
+var1=`echo; ceph df | head -n 5`
+var2=`echo; ceph df | grep rgw.buckets.data`
+updatelog "$var1$var2" $LOGFILE
+# Record GC stats
+var3=`radosgw-admin gc list --include-all | wc -l`
+updatelog "Pending GC's == $var3" $LOGFILE
 
-rawUsed=`ceph df | head -n 3 | tail -n 1 | awk '{print $4}'`
-pendingGC=`radosgw-admin gc list --include-all | wc -l`
-echo "Ending statistics:"
-echo "   %RAW USED ${rawUsed} : Pending GCs ${pendingGC}" 
-date
-
-
+updatelog "$PROGNAME: Done" $LOGFILE
 # DONE
