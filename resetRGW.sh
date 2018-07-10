eca10eb7ab97c6322a09c3a220b2a1fcbbaae77a
John Harrigan <jharriga@redhat.com>
2018-07-10 17:31:01 -0400

    fix typo

--- resetRGW.sh
+++ resetRGW.sh
@@ -28,7 +28,7 @@ function create_pools {
   elif [ "$1" == "ec" ]; then
       cmdtail="erasure myprofile"
       ceph osd erasure-code-profile rm myprofile
-      ceph osd erasure-code-profile set myprofile k=$k m=$m \
+      ceph osd erasure-code-profile set myprofile k=$k m=$m
 ##        crush-failure-domain=osd  # use default of 'host'
       ceph osd crush rule create-erasure default.rgw.buckets.data myprofile
   else
