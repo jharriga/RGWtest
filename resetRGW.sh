bb2c30c99ef69c7f565f57b9d08063c273cb6220
John Harrigan <jharriga@redhat.com>
2019-04-05 14:44:38 -0400

    total redo for runmode support

--- resetRGW.sh
+++ resetRGW.sh
@@ -14,12 +14,12 @@ source "$myPath/vars.shinc"
 function delete_pools {
   for pl in ${pool_list[@]}; do
       if [ $pl != "rbd" ]; then
-          ceph osd pool delete $pl $pl --yes-i-really-really-mean-it
+          $execMON ceph osd pool delete $pl $pl --yes-i-really-really-mean-it
       fi
   done
 
   sleep 5
-  ceph osd crush rule rm default.rgw.buckets.data
+  $execMON ceph osd crush rule rm default.rgw.buckets.data
 }
 
 function create_pools {
@@ -27,36 +27,35 @@ function create_pools {
       cmdtail="replicated"
   elif [ "$1" == "ec" ]; then
       cmdtail="erasure myprofile"
-      ceph osd erasure-code-profile rm myprofile
-      ceph osd erasure-code-profile set myprofile k=$k m=$m
+      $execMON ceph osd erasure-code-profile rm myprofile
+      $execMON ceph osd erasure-code-profile set myprofile k=$k m=$m
 ##        crush-failure-domain=osd  # use default of 'host'
-      ceph osd crush rule create-erasure default.rgw.buckets.data myprofile
+      $execMON ceph osd crush rule create-erasure default.rgw.buckets.data myprofile
   else
       echo "unknown value for REPLICATION in create_pools"; exit
   fi
 
   for pl in ${pool_list[@]}; do
       if [ $pl == "default.rgw.buckets.data" ]; then
-          ceph osd pool create $pl $pg_data $cmdtail
+          $execMON ceph osd pool create $pl $pg_data $cmdtail
           if [ "$1" == "rep" ]; then
-              ceph osd pool set $pl size "${numREPLICAS}"
+              $execMON ceph osd pool set $pl size "${numREPLICAS}"
           fi
       elif [ $pl == "default.rgw.buckets.index" ]; then
-          ceph osd pool create $pl $pg_index replicated
-          ceph osd pool set $pl size "${numREPLICAS}"
+          $execMON ceph osd pool create $pl $pg_index replicated
+          $execMON ceph osd pool set $pl size "${numREPLICAS}"
       else
-          ceph osd pool create $pl $pg replicated
-          ceph osd pool set $pl size "${numREPLICAS}"
+          $execMON ceph osd pool create $pl $pg replicated
+          $execMON ceph osd pool set $pl size "${numREPLICAS}"
       fi
   done
 
-  CEPH_VERSION=`ceph --version`
   # enable RGW on the pools for RHCS 3.x builds
   if echo $CEPH_VERSION | grep -q "10.2." ; then
     echo "Skip pool enable for 2.5 versions"
   else
     for pool in $(rados lspools); do
-       ceph osd pool application enable $pool rgw
+       $execMON ceph osd pool application enable $pool rgw
     done
   fi
 }
@@ -67,6 +66,12 @@ function create_pools {
 echo "$PROGNAME: Running with these values:"
 echo "RGWhostname=$RGWhostname r=$REPLICATION k=$k m=$m pgdata=$pg_data pgindex=$pg_index \
       pg=$pg f=$fast_read"
+
+echo "runmode=$runmode"
+if [ $runmode == “containerized” ]; then
+    nt_start=$(ssh root@RGWhostname < Utils/thr_time.sh)
+    echo $nt_start
+fi
 
 echo "Stopping RGWs"
 ansible -m shell -a 'systemctl stop ceph-radosgw@rgw.`hostname -s`.service' rgws
@@ -84,16 +89,21 @@ echo "Starting RGWs"
 ansible -m shell -a 'systemctl start ceph-radosgw@rgw.`hostname -s`.service' rgws
 
 echo "Creating User - which generates a new Password"
-ssh $RGWhostname 'radosgw-admin user create --uid=johndoe --display-name="John Doe" --email=john@example.com' &&
-ssh $RGWhostname 'radosgw-admin subuser create --uid=johndoe --subuser=johndoe:swift --access=full' 
+$execRGW 'radosgw-admin user create --uid=johndoe --display-name="John Doe" --email=john@example.com' &&
+$execRGW 'radosgw-admin subuser create --uid=johndoe --subuser=johndoe:swift --access=full' 
 
 # Edit the Password into the XML workload files
 echo "inserting new password into XML files $FILLxml, $EMPTYxml, $RUNTESTxml"
-key=$(ssh $RGWhostname 'radosgw-admin user info --uid=johndoe | grep secret_key' | tail -1 | awk '{print $2}' | sed 's/"//g')
+key=$($execRGW 'radosgw-admin user info --uid=johndoe | grep secret_key' | tail -1 | awk '{print $2}' | sed 's/"//g')
 sed  -i "s/password=.*;/password=$key;/g" ${FILLxml}
 sed  -i "s/password=.*;/password=$key;/g" ${EMPTYxml}
 sed  -i "s/password=.*;/password=$key;/g" ${RUNTESTxml}
 
-echo "$PROGNAME: Done"	
+if [ $runmode == “containerized” ]; then
+    nt_end=$(ssh root@RGWhostname < Utils/thr_time.sh)
+    echo $nt_end
+fi
+
+echo "$PROGNAME: Done"
 
 # DONE
